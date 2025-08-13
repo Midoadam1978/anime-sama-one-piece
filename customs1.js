5b4ed204c1e99003a9d536fa3188d9b29bd21e67
Midoadam1978 <xcloudmoney2@gmail.com>
2025-08-13 15:21:51 +0100

    Update customs1.js

--- customs1.js
+++ customs1.js
@@ -6,6 +6,6 @@ button.className = "btn1";
 
 button.addEventListener("click", function() {
   
-  window.open("https://v5.animesama.biz/26-one-piece.html", "_blank");
+  window.open("https://anime-sama.vip/voiranime/tags/one%20piece/", "_blank");
 });
 document.getElementById("btncnt1").appendChild(button);
